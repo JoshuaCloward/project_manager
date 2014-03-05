1880260139c94b0b1b9761e5007bfe7837a37e5e
DPL-Delta <dpl-delta@dpl-deltas-imac.domain.actdsltmp>
2014-03-05 02:49:30 -0700

    Adds sign in, sign up, sign out functionality with devise. Adds dismissable alerts.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :projects
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :project_id
   # attr_accessible :title, :body
 end
